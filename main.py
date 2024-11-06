edbc5eb67d5d13f8c5cda91bf37a3e2e373e418a
victor-hermogenes <victorltrgh@gmail.com>
2024-11-06 17:47:49 -0300

    first scope

---
+++ main.py
@@ -0,0 +1,23 @@
+import qrcode
+import qrcode.constants
+
+def create_qr_code(link, output_file="qr_code.png"):
+    try:
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=10,
+            border=4,
+        )
+        
+        qr.add_data(link)
+        qr.make(fit=True)
+        
+        img = qr.make_image(fill="black", back_color="white")
+        
+        img.save(output_file)
+        print(f"QR Code saved as {output_file}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+create_qr_code("https://www.instagram.com/kethrinsallyfotos?igsh=OTdkajZxdWI2NHU2")
